1a2b948232137ed10029cfeecd725702d63e004b
mcruzloya <cruzloya@stanford.edu>
2023-01-16 23:34:36 -0800

    Added prior-posterior overlap check.

--- botrana.R
+++ botrana.R
@@ -9,16 +9,15 @@ library('mcmcplots')
 library('MCMCvis')
 
 # Read data.
-data <- read.csv("MDR_pipiens.csv")
+data <- read.csv("briere_data_l_botrana.csv")
 data
 
-plot(data$T, 1 / data$value, pch=20, xlab="Temperature [°C]",
-     ylab="Developmental Rate (eggs)", col="steelblue", xlim=c(10, 40),
-     ylim=c(0,0.12))
+plot(data$T, 1 / data$pupae, pch=20, xlab="Temperature [°C]",
+     ylab="Developmental Rate (eggs)", col="steelblue")
 
 # We know what temperatures mean. But sometimes our model has parameters that
 # don't have a straightforward interpretation. To set a reasonable prior 
-# distribution for these parameters, it's a good idea to plot our function.
+# distributions for these parameters, it's a good idea to plot our function.
 briere <- function(T, Tmin, Tmax, c) {
   result <- c * T * (T - Tmin) * sqrt((Tmax - T) * (T > Tmin) * (T < Tmax)  )
   return(result)
@@ -27,7 +26,7 @@ briere <- function(T, Tmin, Tmax, c) {
 T <- seq(5, 40, 0.01)
 
 # Pick some ballpark reasonable values of Tmin and Tmax,
-Tmin = 10
+Tmin = 8
 Tmax = 37
 
 # Try different values of c to get an idea of what this parameter does to the
@@ -45,9 +44,6 @@ plot(T, briere(T, Tmin, Tmax, 0.0001), type="l")
 # Plot Topt for these parameters to check calculation is correct. We will need
 # this formula later to find optimal temperature from fitted model parameters.
 abline(v=(4*Tmax + 3*Tmin)/10 + sqrt(4 * Tmax^2 + (9/4) * Tmin^2 - 4*Tmax*Tmin) / 5)
-## Note: This is the formula for the Briere model. It will not work for other
-## models.
-
 
 # Simple model with uniform priors.
 sink("briere_unif.txt")
@@ -58,7 +54,7 @@ cat("
     c ~ dunif(0, 0.01)
     Tmin ~ dunif(0, 20)
     Tmax ~ dunif(28, 45)
-    sigma ~ dunif(0, 0.2)
+    sigma ~ dunif(0, 0.5)
     tau <- 1 / (sigma * sigma)
     
     ## Likelihood
@@ -81,9 +77,9 @@ cat("
     
     ## Priors
     c ~ dunif(0, 0.001)
-    Tmin ~ dnorm(10, 1 / 25) # 95% likely to be in (0, 20)
-    Tmax ~ dnorm(35, 1 / 25) # 95% likely to be in (25, 45)
-    sigma ~ dunif(0, 0.2)
+    Tmin ~ dnorm(10, 1 / 25) # Assumes Tmin has a 95% chance to be in interval (0, 20).
+    Tmax ~ dnorm(35, 1 / 25) # Assumes Tmax has a 95% chance to be in interval (25, 45).
+    sigma ~ dunif(0, 0.5)
     tau <- 1 / (sigma * sigma)
     
     ## Likelihood
@@ -99,13 +95,12 @@ cat("
     ")
 sink()
 
-##### Calculate initial values for MCMC.
-## We are picking random values so that every chain will start at a different place. 
+##### inits Function
 inits<-function(){list(
   c = runif(0.00001, 0.001),
   Tmax = runif(1, min=35, max=40),
   Tmin = runif(1, min=5, max=10),
-  sigma = runif(1, min=0.001, max=0.01))}
+  sigma = runif(1, min=0.01, max=0.15))}
 
 ##### Parameters to Estimate
 parameters <- c("c", "Tmin", "Tmax","sigma", "Topt")
@@ -118,36 +113,33 @@ nt <- 8 # thinning rate - jags saves every nt iterations in each chain
 nc <- 3 # number of chains
 
 ##### Organize Data for JAGS
-trait <- 1 / data$value
+trait <- 1 / data$pupae
 N.obs <- length(trait)
 temp <- data$T
 
 ##### Bundle Data
 jag.data<-list(trait = trait, N.obs = N.obs, temp = temp)
 ##### Run JAGS
-unif.out <- jags(data=jag.data, inits=inits, parameters.to.save=parameters, model.file="briere_unif.txt",
+pupae.out <- jags(data=jag.data, inits=inits, parameters.to.save=parameters, model.file="briere_unif.txt",
                      n.thin=nt, n.chains=nc, n.burnin=nb, n.iter=ni, DIC=TRUE, working.directory=getwd())
 
+round(pupae.out$BUGSoutput$summary, 4)
+mcmcplot(pupae.out)
 
-# Examine output.
-unif.out
-round(unif.out$BUGSoutput$summary, 6)
-mcmcplot(unif.out)
-save(unif.out, file = "MDR_Cpip_jags_unif.Rdata")
-
-
-chains.unif <- MCMCchains(unif.out, params=c("Tmin", "Tmax", "c"))
+chains.unif <- MCMCchains(pupae.out, params=c("Tmin", "Tmax", "c"))
 temps <- seq(5, 45, 0.1) 
 curves <- apply(chains.unif, 1, function(x) briere(temps, x[1], x[2], x[3]))
 
+# Get mean curve and 2.5% and 97.5% quantiles (for CI).
 meancurve <- apply(curves, 1, mean)
 CI <- apply(curves, 1, quantile, c(0.025, 0.975))
 
-
-plot(data$T, 1 / data$value, xlab="Temperature [°C]",
-     ylab="MDR", xlim=c(5, 45), ylim=c(0, 0.16), pch=20, col="steelblue",
+plot(data$T, 1 / data$pupae, xlab="Temperature [°C]",
+     ylab="pupae development rate", xlim=c(5, 45), ylim=c(0, 0.16), pch=20, col="steelblue",
      main="Uniform priors")
 
+
+# Plot mean prediction and 95% credible interval.
 lines(temps, meancurve, col="black")
 lines(temps, CI[1,], col="gray")
 lines(temps, CI[2,], col="gray")
@@ -157,42 +149,40 @@ lines(temps, CI[2,], col="gray")
 prior.Tmin <- runif(125000, 0, 20) # 125000: Number of samples needs to match MCMC chain.
 prior.Tmax <- runif(125000, 28, 45)
 prior.c <- runif(125000, 0, 0.01)
-prior.sigma <- runif(125000, 0, 0.2)
+prior.sigma <- runif(125000, 0, 0.5)
 
 # Plot prior along with posterior.
-MCMCtrace(unif.out, params=c("Tmin", "Tmax", "c", "sigma"), 
+MCMCtrace(pupae.out, params=c("Tmin", "Tmax", "c", "sigma"), 
           priors=cbind(prior.Tmin, prior.Tmax, prior.c, prior.sigma))
 
 
+
 ##### Run JAGS
-inf.out <- jags(data=jag.data, inits=inits, parameters.to.save=parameters, model.file="briere_inf.txt",
+pupae.out.inf <- jags(data=jag.data, inits=inits, parameters.to.save=parameters, model.file="briere_inf.txt",
                   n.thin=nt, n.chains=nc, n.burnin=nb, n.iter=ni, DIC=TRUE, working.directory=getwd())
 
-inf.out
-round(inf.out$BUGSoutput$summary, 6) # Add more digits so we can see results for c.
-# Save results for later use.
-#save(inf.out, file = "MDR_Cpip_jags_inf.Rdata")
-mcmcplot(inf.out)
+round(pupae.out.inf$BUGSoutput$summary, 4)
+mcmcplot(pupae.out.inf)
 
 
-chains.inf <- MCMCchains(inf.out, params=c("Tmin", "Tmax", "c"))
+chains.inf <- MCMCchains(pupae.out.inf, params=c("Tmin", "Tmax", "c"))
 temps <- seq(5, 45, 0.1) 
 curves <- apply(chains.inf, 1, function(x) briere(temps, x[1], x[2], x[3]))
 
-# Find mean curve and credible intervals.
+
+
+# Get mean curve and 2.5% and 97.5% quantiles (for CI).
 meancurve <- apply(curves, 1, mean)
 CI <- apply(curves, 1, quantile, c(0.025, 0.975))
 
-plot(data$T, 1 / data$value, xlab="Temperature [°C]",
-     ylab="MDR", xlim=c(5, 45), ylim=c(0, 0.16), pch=20, col="steelblue",
+plot(data$T, 1 / data$pupae, xlab="Temperature [°C]",
+     ylab="pupae development rate", xlim=c(5, 45), ylim=c(0, 0.16), pch=20, col="steelblue",
      main="Informative priors")
 
-# Plot mean curve and 95% credible interval.
+# Plot mean prediction and credible interval.
 lines(temps, meancurve, col="black")
 lines(temps, CI[1,], col="gray")
 lines(temps, CI[2,], col="gray")
-
-dim(curves)
 
 ## Check prior-posterior overlap
 # Generate samples from priors.
@@ -201,5 +191,6 @@ prior.Tmax <- rnorm(125000, 35, 5)
 prior.c <- runif(125000, 0, 0.001)
 prior.sigma <- runif(125000, 0, 0.2)
 
-MCMCtrace(inf.out, params=c("Tmin", "Tmax", "c", "sigma"), 
+MCMCtrace(pupae.out.inf, params=c("Tmin", "Tmax", "c", "sigma"), 
           priors=cbind(prior.Tmin, prior.Tmax, prior.c, prior.sigma))
+
